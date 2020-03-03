4f94ba784ec1d5d4ea195212b6927ad7762a6e59
Cayo <cayomesquita@gmail.com>
2020-03-03 07:03:02 -0800

    Add deep link with lib uni_links

--- android/app/src/main/kotlin/edu/cayo/flutter_pactice/MainActivity.kt
+++ android/app/src/main/kotlin/edu/cayo/flutter_pactice/MainActivity.kt
@@ -1,12 +1,15 @@
 package edu.cayo.flutter_pactice
 
+import android.os.Bundle
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
+
 }
